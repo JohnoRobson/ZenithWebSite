5c468d0b212a5d9f90a1d7fdb5dac4571dda090d
johnorobson <johnathan.g.robson@gmail.com>
2017-02-07 04:19:46 -0800

    first commit

---
+++ ZenithDataLib/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithDataLib.Models {
+    public class Event {
+        [Key]
+        public int EventId;
+        public DateTime EventFromDate;
+        public DateTime EventToDate;
+        public string EnteredByUsername;
+        public int Activity;
+        public DateTime CreationDate;
+        public bool IsActive;
+    }
+}
